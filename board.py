d614dd4ef9a75e2dbd3a00bbc607ddd78e8c7e3a
Ari <nanoticity@gmail.com>
2023-10-21 21:44:13 -0700

    Win detection, tie is not yet handled

--- board.py
+++ board.py
@@ -6,7 +6,7 @@ class Board:
     SIZE = [600, 600]
     def __init__(self, display):
         self.display = display
-        self.board = [["x", " ", " "],
+        self.board = [[" ", " ", " "],
                       [" ", " ", " "],
                       [" ", " ", " "]]
     
@@ -20,6 +20,8 @@ class Board:
             for col in range(len(self.board[row])):
                 if self.board[row][col] == "x":
                     self.draw_x((row, col))
+                elif self.board[row][col] == "o":
+                    self.draw_o((row, col))
     
     def draw_x(self, pos):
         middle_pos = (pos[1] * (Board.SIZE[0] / 3) + Board.SIZE[0] / 6, pos[0] * (Board.SIZE[1] / 3) + Board.SIZE[1] / 6) 
@@ -30,3 +32,46 @@ class Board:
         # 2. box * 100
         # box = 500
         pg.draw.polygon(self.display, Color.color("black"), points, 10)
+    
+    def draw_o(self, pos):
+        middle_pos = (pos[1] * (Board.SIZE[0] / 3) + Board.SIZE[0] / 6, pos[0] * (Board.SIZE[1] / 3) + Board.SIZE[1] / 6)
+        pg.draw.circle(self.display, Color.color("black"), middle_pos, 75, 10)
+        
+    def _who_wins(self):
+        for row in self.board:
+            if row[0] == row[1] and row[0] == row[2]:
+                return row[0]
+        for col in range(len(self.board[0])):
+            if self.board[0][col] == self.board[1][col] and self.board[0][col] == self.board[2][col]:
+                return self.board[0][col]
+        if self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2]:
+            return self.board[0][0]
+        if self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0]:
+            return self.board[0][2]
+        return None
+    
+    def who_wins(self):
+        w = self._who_wins()
+        if w == " " or w == None:
+            return None
+        else:
+            return w
+
+if __name__ == "__main__":
+    b = Board("")
+    b.board = [["x", "o", "x"],
+               ["x", "o", "o"],
+               ["x", "x", "o"]]
+    assert b.who_wins() == "x"
+    b.board = [["o", "o", "x"],
+               ["o", "o", "o"],
+               ["x", "x", "o"]]
+    assert b.who_wins() == "o"
+    b.board = [["o", "o", "x"],
+               ["x", "o", "o"],
+               ["x", "x", "o"]]
+    assert b.who_wins() == "o"
+    b.board = [["o", "o", "x"],
+               ["x", "o", "o"],
+               ["o", "x", "x"]]
+    assert b.who_wins() == None
--- game.py
+++ game.py
@@ -5,23 +5,46 @@ from mouse import *
 pg.init()
 
 class Game:
+    def reset(self):
+        self.playerx_turn = True
+        self.board = Board(self.screen)
     def __init__(self):
         self.screen = pg.display.set_mode(Board.SIZE)
-
-        self.board = Board(self.screen)
+        self.reset()
         self.mouse = Mouse()
-    
+        self.clicked = False
+    def text(self, to_write, pos, size, color):
+        font = pg.font.Font(None, size)
+        text = font.render(to_write, True, color)
+        textpos = text.get_rect(centerx = pos[0], centery = pos[1])
+        self.screen.blit(text, textpos)
     def run(self):
         run = True
         while run:
             for e in pg.event.get():
                 if e.type == pg.QUIT:
                     run = False
-                if e.type == pg.MOUSEBUTTONDOWN:
+                if e.type == pg.KEYDOWN:
+                    self.reset()
+                if e.type == pg.MOUSEBUTTONDOWN and not self.clicked:
                     b = self.mouse.which_box()
-                    self.board.board[b[0]][b[1]] = "x"
+                    if self.board.board[b[0]][b[1]] == " ":
+                        if self.playerx_turn:
+                            self.board.board[b[0]][b[1]] = "x"
+                        else:
+                            self.board.board[b[0]][b[1]] = "o"
                     
+                        self.playerx_turn = not self.playerx_turn
+                        winner = self.board.who_wins()
+                        if winner != None:
+                            self.screen.fill(Color.color("white"))
+                            self.text("The winner is " + winner.upper(), (300, 300), 50, Color.color("black"))
+                            pg.display.update()
+                            pg.time.wait(2000)
+                            self.reset()
+                    self.clicked = True
             self.screen.fill(Color.color("white"))
             self.board.draw()
             pg.display.update()
+            self.clicked = False
     
